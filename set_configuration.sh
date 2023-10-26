6c5e85f9ffc6885208ce7df7bd34bfe7e887f343
root <root@drive.nexlesoft.com>
2023-10-26 16:14:01 +0700

    update all

--- set_configuration.sh
+++ set_configuration.sh
@@ -2,11 +2,11 @@
 
 set -x
 
-docker exec -u www-data app-server php occ --no-warnings config:system:get trusted_domains >> trusted_domain.tmp
+docker exec -u www-data app-server php occ --no-warnings config:system:get drive.nexlesoft.com >> trusted_domain.tmp
 
 if ! grep -q "nginx-server" trusted_domain.tmp; then
     TRUSTED_INDEX=$(cat trusted_domain.tmp | wc -l);
-    docker exec -u www-data app-server php occ --no-warnings config:system:set trusted_domains $TRUSTED_INDEX --value="nginx-server"
+    docker exec -u www-data app-server php occ --no-warnings config:system:set drive.nexlesoft.com $TRUSTED_INDEX --value="nginx-server"
 fi
 
 rm trusted_domain.tmp
